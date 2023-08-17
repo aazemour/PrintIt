feee63fd67b10ec52348566a01b844167c874f77
AAZemour <aa.zemour@gmail.com>
2023-08-17 10:09:34 +0200

    Finalaisation du projet

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,44 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const slideImage = document.getElementById('image');
+const tagLine = document.getElementById('tagLine');
+const dots = document.querySelectorAll('.dot');
+let currentIndex = 0;
+
+function updateSlide() {
+  slideImage.src = "./assets/images/slideshow/" + slides[currentIndex].image;
+  tagLine.innerHTML = slides[currentIndex].tagLine;
+
+	dots.forEach((dot, index) => {
+    if (index === currentIndex) {
+      dot.classList.add('dot_selected');
+    } else {
+      dot.classList.remove('dot_selected');
+	}
+	
+ });
+}
+
+arrowLeft.addEventListener('click', function() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSlide();
+});
+
+arrowRight.addEventListener('click', function() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlide();
+});
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', function() {
+    currentIndex = index;
+    updateSlide();
+  });
+});
+
+updateSlide();
+
